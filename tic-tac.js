09c2988d92d0a0024d1e570eedbe9bacb597602a
Elias Weinraub <eweinraub16@gmail.com>
2020-11-18 12:58:27 -0500

    working reset button, slightly styled

--- tic-tac.js
+++ tic-tac.js
@@ -1,15 +1,6 @@
 const boardTiles = document.querySelectorAll('.boardTile')
 const boardTokens = document.querySelectorAll('.boardToken')
-
-const gameBoard = (() => {
-    board: [[1, 2, 3], 
-            [4, 5, 6], 
-            [7, 8, 9]]
-
-    //init game
-    //sets up listeners
-    
-})()
+const resetButton = document.querySelector('#resetButton')
 
 const PlayerData = (() => {
     const Player = (name, token) => {
@@ -21,73 +12,101 @@ const PlayerData = (() => {
     const playerOne = Player('Elias', 'X');
     const playerTwo = Player('David', 'O');
 
-    let currentPlayer = playerOne;
-
+    let currentPlayer = playerTwo;
+    
     return {
         playerOne,
         playerTwo,
-        currentPlayer
+        currentPlayer,
     }
 })()
 
-
-//gets initialized in gameboard setup?
-boardTiles.forEach((tile) => {
-    function tokenPlacement() {
-        let boardToken = tile.querySelector('.boardToken');
-        let selection = tile.id;
+const gameSetup = (() => {
     
+    function togglePlayer() {
         PlayerData.currentPlayer === PlayerData.playerOne ? PlayerData.currentPlayer = PlayerData.playerTwo : PlayerData.currentPlayer = PlayerData.playerOne;
-    
-        PlayerData.currentPlayer.selections.push(Number(selection))
-        boardToken.innerHTML = PlayerData.currentPlayer.token;
-        checkWinner(PlayerData.currentPlayer.selections);
     }
 
-    function onetime(node, type) {
-        // create event
-        node.addEventListener('click', function(e) {
-            // remove event
-            e.target.removeEventListener(e.type, tokenPlacement);
+    function init() {
+
+        //Adds event listeners to board tiles
+            boardTiles.forEach((tile) => {
+                function tokenPlacement() {
+                    let boardToken = tile.querySelector('.boardToken');
+                    let selection = tile.id;
+                            
+                    togglePlayer()
+
+                    PlayerData.currentPlayer.selections.push(Number(selection))
+                    boardToken.innerHTML = PlayerData.currentPlayer.token;
+                    checkWinner(PlayerData.currentPlayer.selections);
+
+                }
+        
+        //Prevents click events on tiles from happening more than once
+            function onetime(node, type) {
+                node.addEventListener('click', function(e) {
+                    e.target.removeEventListener(e.type, tokenPlacement);
+                });
+            }
+        
+            tile.addEventListener('click', tokenPlacement);
+        
+            onetime(tile, 'click');
+        })
+    }
+    
+    //Returns true if any combo of player selections array matches a winning combo
+    function arrayEquals(a, b) {
+        return Array.isArray(a) && 
+        Array.isArray(b) && 
+        a.every(item => b.indexOf(item) !== -1);
+    }
+    
+    function checkWinner() {
+        const winningCombos = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ]
+    
+    //Announces winner
+        winningCombos.forEach((combo) => {
+            let currentSetup = PlayerData.currentPlayer.selections.sort();
+    
+            if(arrayEquals(combo, currentSetup)) {
+                alert(`Congrats ${PlayerData.currentPlayer.name} you win!`)
+            }
+            else {
+                console.log(`It's ${PlayerData.currentPlayer.name}'s turn`)
+            }
         });
     }
-
-    tile.addEventListener('click', tokenPlacement);
-
-    onetime(tile, 'click');
-})
-
-
-
-//things that need to be modulized
-function arrayEquals(a, b) {
-  return Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index]);
-}
-
-function checkWinner() {
-    const winningCombos = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]
-    ]
-
     
-    winningCombos.forEach((combo) => {
-        let currentSetup = PlayerData.currentPlayer.selections.sort();
+    init();
 
-        if(arrayEquals(combo, currentSetup)) {
-            alert(`Congrats ${PlayerData.currentPlayer.name} you win!`)
-        }
-        else {
-            console.log('not so well')
-        }
-    });
-}
+    return {
+        checkWinner,
+        arrayEquals,
+        init,
+        togglePlayer
+    }
+})();
+
+resetButton.addEventListener('click', () => {
+    PlayerData.playerOne.selections = [];
+    PlayerData.playerTwo.selections = [];
+
+    boardTokens.forEach((token) => {
+        token.innerHTML = '';
+    })
+
+    gameSetup.init();
+    PlayerData.currentPlayer = PlayerData.playerTwo;
+
+})
