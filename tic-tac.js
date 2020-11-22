b131b21a8aaaf01511d65c200e92c345380c2dcc
Elias Weinraub <eweinraub16@gmail.com>
2020-11-22 12:32:10 -0500

    logic works, buttons work

--- tic-tac.js
+++ tic-tac.js
@@ -1,6 +1,8 @@
 const boardTiles = document.querySelectorAll('.boardTile')
 const boardTokens = document.querySelectorAll('.boardToken')
 const resetButton = document.querySelector('#resetButton')
+const turnText = document.querySelector('#turnText')
+const editNames = document.querySelector('#editNames')
 
 const PlayerData = (() => {
     const Player = (name, token) => {
@@ -9,10 +11,10 @@ const PlayerData = (() => {
         return {name, selections, token}
     }
     
-    const playerOne = Player('Elias', 'X');
-    const playerTwo = Player('David', 'O');
+    const playerOne = Player('Player 1', 'X');
+    const playerTwo = Player('Player 2', 'O');
 
-    let currentPlayer = playerTwo;
+    let currentPlayer = playerOne;
     
     return {
         playerOne,
@@ -25,32 +27,34 @@ const gameSetup = (() => {
     
     function togglePlayer() {
         PlayerData.currentPlayer === PlayerData.playerOne ? PlayerData.currentPlayer = PlayerData.playerTwo : PlayerData.currentPlayer = PlayerData.playerOne;
+        turnText.innerHTML = `It's ${PlayerData.currentPlayer.name}'s turn`;
     }
 
     function init() {
-
+        
+        turnText.innerHTML = `It's ${PlayerData.currentPlayer.name}'s turn`;
+        
         //Adds event listeners to board tiles
             boardTiles.forEach((tile) => {
                 function tokenPlacement() {
                     let boardToken = tile.querySelector('.boardToken');
                     let selection = tile.id;
                             
-                    togglePlayer()
-
-                    PlayerData.currentPlayer.selections.push(Number(selection))
                     boardToken.innerHTML = PlayerData.currentPlayer.token;
+                    PlayerData.currentPlayer.selections.push(Number(selection));
                     checkWinner(PlayerData.currentPlayer.selections);
-
                 }
         
         //Prevents click events on tiles from happening more than once
             function onetime(node, type) {
                 node.addEventListener('click', function(e) {
                     e.target.removeEventListener(e.type, tokenPlacement);
+                    e.target.removeEventListener(e.type, togglePlayer);
                 });
             }
-        
+
             tile.addEventListener('click', tokenPlacement);
+            tile.addEventListener('click', togglePlayer);
         
             onetime(tile, 'click');
         })
@@ -80,33 +84,43 @@ const gameSetup = (() => {
             let currentSetup = PlayerData.currentPlayer.selections.sort();
     
             if(arrayEquals(combo, currentSetup)) {
-                alert(`Congrats ${PlayerData.currentPlayer.name} you win!`)
+                console.log(`Congrats ${PlayerData.currentPlayer.name} you win!`)
+                alert(`Congrats ${PlayerData.currentPlayer.name} you win!`);
+                reset();
+            }
+            else if(PlayerData.playerOne.selections.length === 5){
+                alert(`It's a tie, you both lose!`);
+                reset();
             }
             else {
-                console.log(`It's ${PlayerData.currentPlayer.name}'s turn`)
+                console.log('good good')
             }
         });
     }
+
+    function reset() {
+        window.location.reload();
+    }
+
+    function editName() {
+        let newNameOne = prompt("What's the first player's name?");
+        let newNameTwo = prompt("What's the second player's name?");
     
+        PlayerData.playerOne.name = newNameOne;
+        PlayerData.playerTwo.name = newNameTwo;
+        turnText.innerHTML = `It's ${PlayerData.currentPlayer.name}'s turn`;
+    }
+    
+    editNames.addEventListener('click', editName);
+    resetButton.addEventListener('click', reset)
     init();
 
     return {
         checkWinner,
         arrayEquals,
         init,
-        togglePlayer
+        togglePlayer,
+        reset,
+        editName
     }
 })();
-
-resetButton.addEventListener('click', () => {
-    PlayerData.playerOne.selections = [];
-    PlayerData.playerTwo.selections = [];
-
-    boardTokens.forEach((token) => {
-        token.innerHTML = '';
-    })
-
-    gameSetup.init();
-    PlayerData.currentPlayer = PlayerData.playerTwo;
-
-})
